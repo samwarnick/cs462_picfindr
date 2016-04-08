2cff263fc1908c4941f5c1d28094d8a695ac91bc
gkrieg <gkrieg13@gmail.com>
2016-04-08 13:02:14 -0600

    clarify stuff kinda added

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import config from './webpack.config.babel.js';
 import request from 'request';
 import bodyParser from 'body-parser';
 import fs from 'fs';
+import multer from 'multer';
+import Clarifai from './clarifai_node.js';
 var compiler = webpack(config);
 
 import express from 'express';
@@ -11,13 +13,19 @@ import express from 'express';
 var peers = [];
 
 var app = express();
-var token = 'PLuoYVKO707urwc5TTKTNkR6j5SfFZ';
+var token = 'YsXUX8HJw0RXM5JkwWEfrT9yJCa6sh';
 var tags = {};
+var uploading = multer({
+  dest: __dirname + '/uploads/',
+});
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
+
+// Clarifai.initAPI("pRH0rURL4ygLoq2egGvpqO9-f2DrCfAoum2QQJoi","X5pu0BEBKaVLTD_I9U4n1KAyu3rFVHRwk1H_rrFh");
+// Clarifai._requestAccessToken();
 
 app.use(require("webpack-dev-middleware")(compiler, {
     noInfo: true, publicPath: config.output.publicPath
@@ -25,16 +33,16 @@ app.use(require("webpack-dev-middleware")(compiler, {
 app.use(require("webpack-hot-middleware")(compiler));
 app.use(express.static('build'));
 
-app.post('/tag', function(req, res) {
-  console.log(req.body);
-  console.log(req.files);
-  console.log(req);
-  fs.readFile(req.files.displayImage.path,  (err, data) => {
-  // ...
-  var newPath = __dirname + "/uploads/" + req.files.displayImage.name;
-  fs.writeFile(newPath, data,(err) => {
-    request.post('https://api.clarifai.com/v1/tag/', {form:{'encoded_data': newPath}, headers: {Authorization: 'Bearer'+token}}, (err,httpResponse,body) => {
-      console.log('herein the thing');
+app.post('/tag', uploading.single('displayImage'), function(req, res) {
+  console.log('file',req.file);
+  var newPath = req.file.path;
+  request.post('https://api.clarifai.com/v1/token/', {'client_id': "pRH0rURL4ygLoq2egGvpqO9-f2DrCfAoum2QQJoi", 'client_secret': "X5pu0BEBKaVLTD_I9U4n1KAyu3rFVHRwk1H_rrFh", 'grant_type': "client_credentials"}, (err, httpResponse, body) => {
+    console.log('err', err);
+    console.log('first response',httpResponse);
+    console.log('first body',body);
+  });
+    request.post('https://api.clarifai.com/v1/tag/', {form:{'encoded_data': newPath}, headers: {Authorization: 'Bearer '+token}}, (err,httpResponse,body) => {
+      console.log(httpResponse);
       if (httpResponse.status_code === 'OK') {
         var newtags = httpResponse.results[0].result.tag.classes;
         for (var tag in newtags) {
@@ -52,9 +60,7 @@ app.post('/tag', function(req, res) {
         console.log(tags);
       }
     });
-    res.redirect("/");
-  });
-});
+    res.status(200).send("OK");
 });
 
 app.get('/test',(req, res) => {
