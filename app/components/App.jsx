9d88e671640b6fb6a881059f60e8cb9c7a23f70d
samwarnick <sam.t.warnick@gmail.com>
2016-04-10 18:06:37 -0600

    add autocomplete options when searching

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -9,18 +9,20 @@ export default class App extends Component {
     super(props);
 
     let socket = io('http://localhost:8080', {'force new connection': true});
-    this.state = {socket: socket, socket_id: ''};
+    this.state = {socket: socket, socket_id: '', tags: []};
     this.state.socket.on('connected', (data) => {
       this.setState(data);
-      console.log(this.state.socketId);
+    });
+    this.state.socket.on('new tags', (data) => {
+      this.setState(data);
     });
   }
 
   render() {
     return (
       <div>
-        <Navbar socket={this.state.socket} socketId={this.state.socket_id}/>
-        <TagList socket={this.state.socket} />
+        <Navbar socket={this.state.socket} socketId={this.state.socket_id} tags={this.state.tags}/>
+        <TagList tags={this.state.tags}/>
       </div>
     );
   }
